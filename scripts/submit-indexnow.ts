284a4f7f97db605243d4d558fd311f25811d34a3
lotustemplar <filipetheology@gmail.com>
2026-07-18 01:35:45 -0700

    Load env files for IndexNow backfills

--- scripts/submit-indexnow.ts
+++ scripts/submit-indexnow.ts
@@ -1,6 +1,9 @@
-import { submitFullSiteToIndexNow } from "@/lib/indexnow";
+import { loadEnvConfig } from "@next/env";
+
+loadEnvConfig(process.cwd());
 
 async function main() {
+  const { submitFullSiteToIndexNow } = await import("@/lib/indexnow");
   await submitFullSiteToIndexNow();
   console.log("Submitted current public site URLs to IndexNow.");
 }
